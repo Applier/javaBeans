1537445e74ee3b25117ed509f0d2917e329fe766
Applier. <appliercontact@gmail.com>
2014-12-28 21:13:15 +0100

    Create Loisir.java

---
+++ Loisir.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package intergiciels.beans;
+
+/**
+ * @author Jade
+ *
+ */
+public class Loisir {
+	
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+}
