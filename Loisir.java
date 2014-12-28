764ba8270caa61052b6b36d3e124b07e33138705
Applier. <appliercontact@gmail.com>
2014-12-28 21:22:09 +0100

    Update Loisir.java

--- Loisir.java
+++ Loisir.java
@@ -9,14 +9,26 @@ package intergiciels.beans;
  */
 public class Loisir {
 	
+	/* Attributs */
 	private String nom;
+	private String description;
 
+	/* Getters et Setters */
+	
 	public String getNom() {
 		return nom;
 	}
 
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 }
