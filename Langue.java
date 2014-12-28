3762ed98ed91bccf3cc166ad8e04ce52fc8d37f6
Applier. <appliercontact@gmail.com>
2014-12-28 21:28:30 +0100

    Create Langue.java

---
+++ Langue.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package intergiciels.beans;
+
+/**
+ * @author Jade
+ *
+ */
+public class Langue {
+	
+	/* Attributs */
+	private String nom;
+	private String niveau;
+	
+	/* Getters et Setters */
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNiveau() {
+		return niveau;
+	}
+	
+	public void setNiveau(String niveau) {
+		this.niveau = niveau;
+	}
+	
+
+}
