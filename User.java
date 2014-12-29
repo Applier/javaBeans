733d3382b890018725ac403bfb744e5a13b48ac4
Applier. <appliercontact@gmail.com>
2014-12-29 14:24:08 +0100

    Update User.java
    allègement de la classe en regroupant tous les attributs relatifs aux informations personnelles pour le CV dans la classe InfosCV

--- User.java
+++ User.java
@@ -4,7 +4,6 @@
 package intergiciels.beans;
 
 import java.util.Collection;
-import java.util.Date;
 
 /**
  * @author Jade
@@ -20,14 +19,7 @@ public class User {
 	private String mail;
 	private String password;
 	// infos personnelles relatives au CV
-	private int numTel;
-	private Date dateNaissance;
-	private String adresse;
-	private Collection<DomaineCompetences> competences;
-	private Collection<Emploi> emplois;
-	private Collection<Formation> formations;
-	private Collection<Langue> langues;
-	private Collection<Loisir> loisirs;
+	private InfosCV infos;
 	// offres
 	private Collection<Offre> offres;
 	
@@ -65,68 +57,12 @@ public class User {
 		this.password = password;
 	}
 	
-	// numTel
-	public int getNumTel() {
-		return this.numTel;
+	// infos personnelles relatives au CV
+	public InfosCV getInfos() {
+		return infos;
 	}
-	public void setNumTel(int numTel) {
-		this.numTel = numTel;
-	}
-	
-	// dateNaissance
-	public Date getDateNaissance() {
-		return this.dateNaissance;
-	}
-	public void setDateNaissance(Date dateNaissance) {
-		this.dateNaissance = dateNaissance;
-	}
-	
-	// adresse
-	public String getAdresse() {
-		return this.adresse;
-	}
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-	
-	// domainesCompetences
-	public Collection<DomaineCompetences> getCompetences() {
-		return this.competences;
-	}
-	public void setCompetences(Collection<DomaineCompetences> competences) {
-		this.competences = competences;
-	}
-	
-	// emplois
-	public Collection<Emploi> getEmplois() {
-		return this.emplois;
-	}
-	public void setEmplois(Collection<Emploi> emplois) {
-		this.emplois = emplois;
-	}
-	
-	// formations
-	public Collection<Formation> getFormations() {
-		return this.formations;
-	}
-	public void setFormations(Collection<Formation> formations) {
-		this.formations = formations;
-	}
-	
-	// langues
-	public Collection<Langue> getLangues() {
-		return this.langues;
-	}
-	public void setLangues(Collection<Langue> langues) {
-		this.langues = langues;
-	}
-	
-	// loisirs
-	public Collection<Loisir> getLoisirs() {
-		return this.loisirs;
-	}
-	public void setLoisirs(Collection<Loisir> loisirs) {
-		this.loisirs = loisirs;
+	public void setInfos(InfosCV infos) {
+		this.infos = infos;
 	}
 	
 	// offres
@@ -138,72 +74,10 @@ public class User {
 	}
 	
 	
-	/* Operations complementaires sur les collections */
-	
-	// ajouter
-	
-	public void addDomaineCompetences(DomaineCompetences domaine) {
-		this.competences.add(domaine);
-	}
-	
-	public void addEmploi(Emploi emploi) {
-		this.emplois.add(emploi);
-	}
-	
-	public void addFormation(Formation formation) {
-		this.formations.add(formation);
-	}
-	
-	public void addLangue(Langue langue) {
-		this.langues.add(langue);
-	}
-	
-	public void addLoisir(Loisir loisir) {
-		this.loisirs.add(loisir);
-	}
-	
+	/* Méthodes complémentaires sur la collection d'offres */
 	public void addOffre(Offre offre) {
 		this.offres.add(offre);
-	}
-	
-	
-	// supprimer
-	
-	public void removeCompetence(DomaineCompetences domaine) {
-		this.competences.remove(domaine);
-	}
-	public void removeAllCompetences() {
-		this.competences.clear();
-	}
-	
-	public void removeEmploi(Emploi emploi) {
-		this.emplois.remove(emploi);
-	}
-	public void removeAllEmplois() {
-		this.emplois.clear();
-	}
-	
-	public void removeFormation(Formation formation) {
-		this.formations.remove(formation);
-	}
-	public void removeAllFormations() {
-		this.formations.clear();
-	}
-	
-	public void removeLangue(Langue langue) {
-		this.langues.remove(langue);
-	}
-	public void removeAllLangues() {
-		this.langues.clear();
-	}
-	
-	public void removeLoisir(Loisir loisir) {
-		this.loisirs.remove(loisir);
-	}
-	public void removeAllLoisirs() {
-		this.loisirs.clear();
-	}
-	
+	}	
 	public void removeOffre(Offre offre) {
 		this.offres.remove(offre);
 	}
@@ -211,4 +85,5 @@ public class User {
 		this.offres.clear();
 	}
 	
+
 }
